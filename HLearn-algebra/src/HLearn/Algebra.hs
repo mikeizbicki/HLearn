cea568b1b1c87266f04d486ebe51e9f4977e27a1
Mike Izbicki <mike@izbicki.me>
2012-12-19 20:47:24 -0800

    Morphism

--- HLearn-algebra/src/HLearn/Algebra.hs
+++ HLearn-algebra/src/HLearn/Algebra.hs
@@ -1,12 +1,14 @@
 -- | This is the base module for the HLearn library.  It exports all the functions / data structures needed.
 
 module HLearn.Algebra
-    ( module HLearn.Algebra.Models
+    ( module HLearn.Algebra.Functions
+    , module HLearn.Algebra.Models
+    , module HLearn.Algebra.Morphism
     , module HLearn.Algebra.Structures
-    , module HLearn.Algebra.Functions
     )
     where
           
-import HLearn.Algebra.Models
-import HLearn.Algebra.Structures
 import HLearn.Algebra.Functions
+import HLearn.Algebra.Models
+import HLearn.Algebra.Morphism
+import HLearn.Algebra.Structures
