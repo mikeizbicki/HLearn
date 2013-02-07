4259f21fa98869cb2518e84fd9e4b0036c774097
Mike Izbicki <mike@izbicki.me>
2013-02-06 16:28:37 -0800

    Added support for plotting multiple distributions on the same graph.

--- HLearn-distributions/src/HLearn/Gnuplot/Distributions.hs
+++ HLearn-distributions/src/HLearn/Gnuplot/Distributions.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 -- | This file contains the functions for plotting distributions using Gnuplot.  The interface is still under heavy construction, so it's not very well documented as of yet.
 
@@ -14,6 +17,7 @@ import qualified Data.Map as Map
 import qualified Data.Vector.Unboxed as VU
 
 import Control.Monad
+import Data.List
 import System.IO
 import System.Process
 
@@ -40,7 +44,22 @@ defPlotParams = PlotParams
     , picFile = "hlearn-distributions.ps"
     }
 
-class PlottableDistribution dist where
+class PlottableDataPoint dp where
+    gensamples :: (dp,dp) -> [dp]
+    
+instance PlottableDataPoint Double where
+    gensamples (start,stop) = fmap (\x -> start+x*(stop-start)/1000) [0..1000]
+
+class 
+    ( Show dp, Ord dp
+    , Show prob
+    , Distribution dist dp prob
+    , PlottableDataPoint dp
+    ) => PlottableDistribution dist dp prob | dist -> dp where
+    
+    minx :: dist -> dp
+    maxx :: dist -> dp
+    
     plotDistribution :: PlotParams -> dist -> IO ()
     plotDistribution params dist = do
         -- Create data file
@@ -60,8 +79,46 @@ class PlottableDistribution dist where
         putStrLn "done."
         return ()
 
+    plotDistributionL :: PlotParams -> [dist] -> IO ()
+    plotDistributionL params distL = do
+        -- Create data file
+        putStrLn "Creating data file..."
+        datah <- openFile (dataFile params) WriteMode
+        hPutStrLn datah $ plotdataL distL
+        hClose datah
+        
+        -- Create gnuplot file
+        putStrLn "Plotting data"
+        gnuh <- openFile (gnuFile params) WriteMode
+        hPutStrLn gnuh $ gnuplotL params distL
+        hClose gnuh
+        
+        -- Run gnuplot, creating picture
+        system $ "gnuplot "++(gnuFile params)
+        putStrLn "done."
+        return ()
+
+    plotdataL :: [dist] -> String
+    plotdataL distL = mconcat 
+                            [show (x::dp) ++ (mconcat
+                                [ " " ++ show (pdf dist x::prob) 
+                                | dist <- distL
+                                ])
+                                ++"\n" 
+                            | x <- plotPoints
+                            ]
+        where
+            minpt = minimum $ fmap minx distL
+            maxpt = maximum $ fmap maxx distL
+            plotPoints = gensamples (minpt,maxpt)
+
     plotdata :: dist -> String
+    plotdata dist = mconcat [show (x::dp) ++ " " ++ show (pdf dist x::prob) ++"\n" | x <- plotPoints]
+        where
+            plotPoints = gensamples (minx dist,maxx dist)
+
     gnuplot  :: PlotParams -> dist -> String
+    gnuplotL  :: PlotParams -> [dist] -> String
     
 {-instance PlottableDistribution (KDE Double) where
     plotdata dist = mconcat [show (x::Double) ++ " " ++ show (pdf dist x::Double) | x <- plotPoints]
@@ -76,7 +133,37 @@ class PlottableDistribution dist where
         ++ "plot '"++(dataFile params)++"' using 1:2 lt 1 lw 4 lc rgb '#ccccff' with filledcurves, \\"
         ++ "     '"++(dataFile params)++"' using 1:2 lt 1 lw 4 lc rgb '#0000ff' with lines"-}
         
-instance (Show label, Show prob, Num prob, Ord prob) => PlottableDistribution (Categorical label prob) where
+instance PlottableDistribution (Gaussian Double) Double Double where
+    
+    minx dist = (meanG dist)-5*(sqrt $ varG dist)
+    maxx dist = (meanG dist)+5*(sqrt $ varG dist)
+    
+    gnuplot params dist
+        =  "set terminal postscript \"Times-Roman\" 25 \n"
+        ++ "set output \"" ++ (picFile params) ++ "\" \n"
+        ++ "unset xtics; unset ytics; unset key \n"
+        ++ "set border 0; set xzeroaxis lt 1; set yzeroaxis lt 1 \n"
+        ++ "plot '"++(dataFile params)++"' using 1:2 lt 1 lw 4 lc rgb '#ccccff' with filledcurves, "
+        ++ "     '"++(dataFile params)++"' using 1:2 lt 1 lw 4 lc rgb '#0000ff' with lines"
+        
+    gnuplotL params distL
+        =  "set terminal postscript \"Times-Roman\" 25 \n"
+        ++ "set output \"" ++ (picFile params) ++ "\" \n"
+        ++ "unset xtics; unset ytics; unset key \n"
+        ++ "set style fill transparent solid 0.5 noborder\n"
+        ++ "set border 0; set xzeroaxis lt 1; set yzeroaxis lt 1 \n"
+        ++ "plot "
+            -- ++(dataFile params)++"' using 1:2 lt 1 lw 4 lc rgb '#ccccff' with filledcurves, "
+        ++ (mconcat . intersperse ", " $ 
+            [ -- "     '"++(dataFile params)++"' using 1:"++show i++" fs solid 1.0 lc rgb '"++bgcolor++"'" -- "' with filledcurves,"
+            "     '"++(dataFile params)++"' using 1:"++show i++" lt 1 lw 4 lc rgb '"++linecolor++"' with lines"
+            | (i,linecolor,bgcolor,_dist) <- zip4 [2..] linecolorL bgcolorL distL
+            ])
+        where 
+            linecolorL = ["#0000ff", "#00ff00", "#ff0000"]
+            bgcolorL   = ["#ccccff", "#ccffcc", "#ffcccc"]
+        
+{-instance (Show label, Show prob, Num prob, Ord prob) => PlottableDistribution (Categorical label prob) label where
     plotdata dist = concat $ do
         (label,prob) <- dist2list dist
         return $ show label++" "++show prob++"\n"
@@ -97,7 +184,7 @@ instance (Show label, Show prob, Num prob, Ord prob) => PlottableDistribution (C
                 then ""
                 else if positiveSamples
                     then "set yrange [0:]\n"
-                    else "set yrange [:0]\n"
+                    else "set yrange [:0]\n"-}
 {-        =  "set terminal postscript \"Times-Roman\" 25 \n"
         ++ "set output \"" ++ (picFile params) ++ "\" \n"
         ++ "unset xtics; unset ytics; unset key\n"
---
+++ HLearn-distributions/src/examples/HomTree-plots/Plots.hs
@@ -0,0 +1,21 @@
+import HLearn.Algebra
+import HLearn.Models.Distributions
+import HLearn.Gnuplot.Distributions
+    
+g1 = train [1,2,3,4] :: Gaussian Double
+g2 = train [2,3,4,5] :: Gaussian Double
+g3 = train [4,5,6,7] :: Gaussian Double
+g4 = train [7,8,9,10] :: Gaussian Double
+
+gfat = train [-3,8] :: Gaussian Double
+
+main = do
+    plotDistributionL (plotFile "g-highoverlap") [g1,g2]
+    plotDistributionL (plotFile "g-lowoverlap") [g1,g3]
+    
+    plotDistributionL (plotFile "g-multidecision-fat") [g1,gfat]
+    plotDistributionL (plotFile "g-multidecision-multi") [g1,g3,g4]
+--     plotDistribution (plotFile "g1") g1
+--     plotDistribution (plotFile "g2") g2
+--     plotDistribution (plotFile "g3") g3
+--     plotDistribution (plotFile "gfat") gfat
