81b9834603bad9aafa23d6e8edf7fde2a3533c03
Mike Izbicki <mike@izbicki.me>
2013-02-15 23:05:09 -0800

    Norms (still need to add vector space, maybe)

--- HLearn-datastructures/src/HLearn/NP-Hard/Partition.hs
+++ HLearn-datastructures/src/HLearn/NP-Hard/Partition.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module HLearn.NPHard.Partition
     where
@@ -21,54 +22,55 @@ import HLearn.DataStructures.BinarySearchTree
 -------------------------------------------------------------------------------
 -- CountingSeq
 
-data CountingSeq a = CountingSeq 
-    { total :: Double
-    , countingseq :: Seq.Seq (Double,a)
+data CountingSeq r a = CountingSeq 
+    { total :: r
+    , countingseq :: Seq.Seq a
     }
     deriving (Read,Show,Eq,Ord)
     
-cs2list :: CountingSeq a -> [(Double,a)]
+cs2list :: CountingSeq r a -> [a]
 cs2list = F.toList . countingseq
     
-singletoncs :: (Double,a) -> CountingSeq a
-singletoncs (w,a) = CountingSeq w $ Seq.singleton (w,a)
+cseq_singleton :: (Norm a r) => a -> CountingSeq r a
+cseq_singleton a = CountingSeq (magnitude a) $ Seq.singleton a
     
-instance F.Foldable CountingSeq where
-    foldr f b (CountingSeq t s) = F.foldr (f . snd) b s
+instance F.Foldable (CountingSeq r) where
+    foldr f b (CountingSeq t s) = F.foldr f b s
     
-instance Triangle (CountingSeq a) (Double,a) where
-    (CountingSeq t s) |> (d,a) = CountingSeq (t+d) (s|>(d,a))
-    (d,a) <| (CountingSeq t s) = CountingSeq (t+d) ((d,a)<|s)
+instance (Num r, Norm a r) => Triangle (CountingSeq r a) a where
+    (CountingSeq t s) |> a = CountingSeq (t+(magnitude a)) (s|>a)
+    a <| (CountingSeq t s) = CountingSeq (t+(magnitude a)) (a<|s)
     
-instance Semigroup (CountingSeq a) where
+instance (Num r) => Semigroup (CountingSeq r a) where
     (CountingSeq t1 s1)<>(CountingSeq t2 s2)=CountingSeq (t1+t2) (s1<>s2)
     
-instance Monoid (CountingSeq a) where
+instance (Num r) => Monoid (CountingSeq r a) where
     mempty = CountingSeq 0 mempty
     mappend = (<>)
 
 -------------------------------------------------------------------------------
 -- data types    
 
-data Partition a r (n::Nat) = Partition
-    { bst :: !(BST (Double,a))
-    , sets :: Map.Map Int [(Double,a)]
+data Partition a (n::Nat) = Partition
+    { bst :: !(BST a)
+    , sets :: Map.Map Int [a]
     }
     deriving (Read,Show,Eq,Ord)
     
-testp = train [(4,"poop"),(3,"the"),(1,"fart"),(1,"fart")] :: Partition String 30
+-- testp = train [(4,"poop"),(3,"the"),(1,"fart"),(1,"fart")] :: Partition String 30
+testp = train [1..100] :: Partition Double 3
     
-bst2partition :: forall n a. (SingI n) => BST (Double,a) -> Partition a n
+bst2partition :: forall a r n. (Norm a r, Num r, Ord r, SingI n) => BST a -> Partition a n
 bst2partition bst = Partition
     { bst = bst
     , sets = bst2sets (fromIntegral $ fromSing (sing :: Sing n)) bst
     }
     
-bst2sets :: Int -> BST (Double,a) -> Map.Map Int [(Double,a)]
+bst2sets :: (Norm a r, Num r, Ord r) => Int -> BST a -> Map.Map Int [a]
 bst2sets n bst = Map.map cs2list $ F.foldr go base bst
     where
         base = Map.fromList [(i,mempty) | i<-[0..n-1]]
-        go (w,a) sets = Map.insertWith (<>) minindex (singletoncs (w,a)) sets
+        go a sets = Map.insertWith (<>) minindex (cseq_singleton a) sets
             where
                 minindex = fst $ argmin (\(k,p) -> total p) $ Map.toList sets
     
@@ -79,10 +81,10 @@ data PartitionParams = PartitionParams
 -------------------------------------------------------------------------------
 -- Algebra
 
-instance (Ord a, SingI n) => Semigroup (Partition a n) where
+instance (Norm a r, Ord r, Num r, SingI n) => Semigroup (Partition a n) where
     p1 <> p2 = bst2partition $ (bst p1) <> (bst p2)
 
-instance (Ord a, SingI n) => Monoid (Partition a n) where
+instance (Norm a r, Ord r, Num r, SingI n) => Monoid (Partition a n) where
     mempty = bst2partition mempty
     mappend = (<>)
 
@@ -92,8 +94,8 @@ instance (Ord a, SingI n) => Monoid (Partition a n) where
 instance Model PartitionParams (Partition a n) where
     getparams _ = PartitionParams
     
-instance (SingI n) => DefaultModel PartitionParams (Partition a n) where
+instance (Norm a r, SingI n) => DefaultModel PartitionParams (Partition a n) where
     defparams = PartitionParams
     
-instance (Ord a, SingI n) => HomTrainer PartitionParams (Double,a) (Partition a n) where
+instance (Norm a r, Ord r, Num r, SingI n) => HomTrainer PartitionParams a (Partition a n) where
     train1dp' _ dp = bst2partition $ train1dp dp
