7fe951e34070b76715b1d71d75839df04269c546
Mike Izbicki <mike@izbicki.me>
2013-02-15 23:05:09 -0800

    Norms (still need to add vector space, maybe)

--- HLearn-algebra/src/HLearn/Algebra/Structures/MetricSpace.hs
+++ HLearn-algebra/src/HLearn/Algebra/Structures/MetricSpace.hs
@@ -8,15 +8,20 @@
 module HLearn.Algebra.Structures.MetricSpace
     where
           
-class MetricSpace m r | m -> r where
+class (Ord m) => Norm m r | m -> r where
+    magnitude :: m -> r
+          
+-- | We assume that the MetricSpace on m is compatible with the ordering on m
+class (Norm m r) => MetricSpace m r | m -> r where
     dist :: m -> m -> r
-    origin :: m
     
-magnitude :: (MetricSpace m r) => m -> r
-magnitude x = dist origin x
+-- magnitude :: (MetricSpace m r) => m -> r
+-- magnitude x = dist origin x
     
 instance MetricSpace Double Double where
-    dist x y = sqrt $ x*x+y*y
-    origin = 0
-    
+    dist x y = abs $ x - y
+
+instance Norm Double Double where
+    magnitude x = abs x
+
 -- instance MetricSpace String Int where
