43368c0c11e5df992eb7e7e7ef2421d9302b52f7
Mike Izbicki <mike@izbicki.me>
2013-04-29 15:40:21 -0700

    Changed Module type class to use TypeFamilies

--- HLearn-algebra/src/HLearn/Algebra/HVector.hs
+++ HLearn-algebra/src/HLearn/Algebra/HVector.hs
@@ -6,10 +6,12 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverlappingInstances #-}
 
 -- | Algebraic instances for the HVector type.  These form the data points used for much of the library.
 module HLearn.Algebra.HVector
     ( module Data.Vector.Heterogenous
+    , L2
     )
     where
 
@@ -17,6 +19,7 @@ import Data.Vector.Heterogenous
 
 import HLearn.Algebra.HomTrainer
 import HLearn.Algebra.Structures.Groups
+import HLearn.Algebra.Structures.MetricSpace
 import HLearn.Algebra.Structures.Modules
 import HLearn.Algebra.Structures.Triangles
 
@@ -40,31 +43,77 @@ instance (Group (HList xs), ValidHVector box xs) => Group (HVector box xs) where
 
 ---------------------------------------
 
-instance LeftOperator r (HList '[]) where
-    r .* HNil = HNil
+instance (HasRing x) => HasRing (HList (x ': xs)) where
+    type Ring (HList (x ': xs)) = Ring x
     
-instance (LeftOperator r x, LeftOperator r (HList xs)) => LeftOperator r (HList (x ': xs)) where
+instance (Module x, Module (HList xs), Ring (HList xs) ~ Ring x) => Module (HList (x ': xs)) where
     r .* (x:::xs) = (r.*x):::(r.*xs)
-
-instance (LeftOperator r (HList xs), ValidHVector box xs) => LeftOperator r (HVector box xs) where
-    r .* hv = vec (undefined::a->box) $ r .* toHList hv
-
-instance (Num r) => LeftModule r (HList '[])
-instance (LeftModule r x, LeftModule r (HList xs)) => LeftModule r (HList (x ': xs))
-instance (LeftModule r (HList xs), ValidHVector box xs) => LeftModule r (HVector box xs)
-
-instance RightOperator r (HList '[]) where
-    HNil *. r = HNil
     
-instance (RightOperator r x, RightOperator r (HList xs)) => RightOperator r (HList (x ': xs)) where
-    (x:::xs) *. r = (x*.r):::(xs*.r)
+instance (HasRing x) => HasRing (HList (x ': '[])) where
+    type Ring (HList (x ': '[])) = Ring x
+    
+instance (Module x) => Module (HList (x ': '[])) where
+    r .* (x:::HNil) = (r.*x):::HNil
 
-instance (RightOperator r (HList xs), ValidHVector box xs) => RightOperator r (HVector box xs) where
-    hv*.r = vec (undefined::a->box) $ toHList hv *. r
+---------------------------------------
 
-instance (Num r) => RightModule r (HList '[])
-instance (RightModule r x, RightModule r (HList xs)) => RightModule r (HList (x ': xs))
-instance (RightModule r (HList xs), ValidHVector box xs) => RightModule r (HVector box xs)
+newtype L2 xs = L2 xs
+
+instance (Num r) => Abelian (L2 [r])
+instance (Num r) => Monoid (L2 [r]) where
+    mempty = L2 $ repeat 0
+    mappend (L2 xs) (L2 ys) = L2 $ zipWith (+) xs ys
+    
+instance (Num r) => Group (L2 [r]) where
+    inverse (L2 xs) = L2 $ map (*(-1)) xs
+   
+instance (Num r) => HasRing (L2 [r]) where
+    type Ring (L2 [r]) = r
+
+instance (Num r) => Module (L2 [r]) where
+--     type Ring (L2 [r]) = r
+    r .* (L2 xs) = L2 $ map (r*) xs
+
+instance (Floating r) => MetricSpace (L2 [r]) where
+    distance (L2 xs) (L2 ys) = sqrt . sum . map (^2) $ zipWith (-) xs ys
+
+-- instance (Num a) => HasRing a where
+--     type Ring a = a
+-- 
+-- instance Monoid Double where
+--     mempty = 0
+--     mappend = (+)
+--     
+-- instance Group Double where
+--     inverse = negate
+-- 
+-- instance Abelian Double
+-- 
+-- instance Module Double where
+--     (.*) = (*)
+
+-- instance (LeftOperator r x, LeftOperator r (HList xs)) => LeftOperator r (HList (x ': xs)) where
+--     r .* (x:::xs) = (r.*x):::(r.*xs)
+-- 
+-- instance (LeftOperator r (HList xs), ValidHVector box xs) => LeftOperator r (HVector box xs) where
+--     r .* hv = vec (undefined::a->box) $ r .* toHList hv
+-- 
+-- instance (Num r) => LeftModule r (HList '[])
+-- instance (LeftModule r x, LeftModule r (HList xs)) => LeftModule r (HList (x ': xs))
+-- instance (LeftModule r (HList xs), ValidHVector box xs) => LeftModule r (HVector box xs)
+-- 
+-- instance RightOperator r (HList '[]) where
+--     HNil *. r = HNil
+--     
+-- instance (RightOperator r x, RightOperator r (HList xs)) => RightOperator r (HList (x ': xs)) where
+--     (x:::xs) *. r = (x*.r):::(xs*.r)
+-- 
+-- instance (RightOperator r (HList xs), ValidHVector box xs) => RightOperator r (HVector box xs) where
+--     hv*.r = vec (undefined::a->box) $ toHList hv *. r
+-- 
+-- instance (Num r) => RightModule r (HList '[])
+-- instance (RightModule r x, RightModule r (HList xs)) => RightModule r (HList (x ': xs))
+-- instance (RightModule r (HList xs), ValidHVector box xs) => RightModule r (HVector box xs)
 
 -------------------------------------------------------------------------------
 -- Training
--- HLearn-algebra/src/HLearn/Algebra/Structures/Modules.hs
+++ HLearn-algebra/src/HLearn/Algebra/Structures/Modules.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -13,141 +14,32 @@
 module HLearn.Algebra.Structures.Modules
     where
 
-import qualified Control.ConstraintKinds as CK
 import Data.List
-import qualified Data.Map as Map
 import HLearn.Algebra.Structures.Groups
-
--------------------------------------------------------------------------------
--- Operators
-
-class (LeftOperator r m, RightOperator r m) => Operator r m
-instance (LeftOperator r m, RightOperator r m) => Operator r m
-
-class LeftOperator r m | m -> r where
-    infix 7 .*
-    (.*) :: r -> m -> m
-
-class RightOperator r m | m -> r where
-    infix 7 *.
-    (*.) :: m -> r -> m
-
-instance RightOperator Integer Integer where (*.) = (*)
-instance LeftOperator Integer Integer where (.*) = (*)
-
-instance RightOperator Int Int where (*.) = (*)
-instance LeftOperator Int Int where (.*) = (*)
-
-instance RightOperator Float Float where (*.) = (*)
-instance LeftOperator Float Float where (.*) = (*)
-
-instance RightOperator Double Double where (*.) = (*)
-instance LeftOperator Double Double where (.*) = (*)
-
-instance (RightOperator a b) => RightOperator a [b] where bs *. a = fmap (*.a) bs
-instance (LeftOperator a b) => LeftOperator a [b] where a .* bs = fmap (a.*) bs
-
--------------------------------------------------------------------------------
--- FreeOp
-
--- | Bug: Technically, the free operator should just require that r be a semigroup and use (<>) to chain the r's together.  But this would make things awkward because the number types aren't instances of semigroup.  Constraining r to be of type Num reduces our generality but makes FreeOp easier to work with in most practical use cases.
-
--- newtype (Num r) => FreeOp r a = FreeOp [(r,a)]
---     deriving (Read,Show)
--- 
--- instance (Num r) => Functor (FreeOp r) where
---     fmap f (FreeOp xs) = FreeOp $ map (\(r,a) -> (r,f a)) xs
--- 
--- instance (Num r) => LeftOperator r (FreeOp r m) where
---     r <| (FreeOp xs) = FreeOp $ map (\(r2,m) -> (r*r2,m)) xs
---     
--- instance (Num r) => RightOperator r (FreeOp r m) where
---     m |> r = r <| m
---     
--- list2freeop :: (Num r) => [a] -> FreeOp r a
--- list2freeop = FreeOp . map (\x -> (1,x))
-
-
 
 -------------------------------------------------------------------------------
 -- Modules
 
+class (Num (Ring m)) => HasRing m where
+    type Ring m
+
 -- | Bug: The module classes have the constraint that r be of type Num.  Technically, this should be a Ring.  But creating a Ring class would be awkward because it would conflict with the Num class and require importing a different Prelude.
-
-class (LeftModule r g, RightModule r g) => Module r g
-instance (LeftModule r g, RightModule r g) => Module r g
-
-class (LeftOperator r g, Num r, Group g, Abelian g) => LeftModule r g
-instance (LeftOperator r g, Num r, Group g, Abelian g) => LeftModule r g
-
-class (RightOperator r g, Num r, Group g, Abelian g) => RightModule r g
-instance (RightOperator r g, Num r, Group g, Abelian g) => RightModule r g
+class (HasRing m, Abelian m, Group m) => Module m where    
+    infix 7 .*
+    (.*) :: (Ring m) -> m -> m
+    
+    {-# INLINE (*.) #-}
+    infix 7 *.
+    (*.) :: m -> (Ring m) -> m
+    m *. r = r .* m
 
 -------------------------------------------------------------------------------
 -- Vector Spaces
 
-class (Module r m, Fractional r) => VectorSpace r m | m -> r where
-    infix 7 ./
-    (./) :: r -> m -> m
-
+class (Module m, Fractional (Ring m)) => VectorSpace m where
     infix 7 /.
-    (/.) :: m -> r -> m
+    {-# INLINE (/.) #-}
+    (/.) :: m -> (Ring m) -> m
     m /. r = m *. (1/r)
 
-class (Ord r, Fractional r) => Norm r m | m -> r where
-    magnitude :: m -> r
-          
-instance (Norm r a) => Norm r [a] where
-    magnitude xs = foldl' (+) 0 $ map magnitude xs
-
-instance Norm Double Double where
-    magnitude x = abs x
-
--------------------------------------------------------------------------------
--- FreeModule
-
-data FreeModParams = FreeModParams
-
-newtype {-(Num r, Ord a) =>-} FreeMod r a = FreeMod (Map.Map a r)
-    deriving (Read,Show,Eq)
-
-instance CK.Functor (FreeMod r) where
-    type FunctorConstraint (FreeMod r) a = (Num r, Ord a)
-    fmap f (FreeMod m) = FreeMod $ Map.mapKeysWith (+) f m
-
-instance CK.Foldable (FreeMod r) where
-    type FoldableConstraint (FreeMod r) a = (Num r, Ord a, Operator r a)
---     foldr f b (FreeMod m) = Map.foldrWithKey (\a r b -> f (r .* a) b) b m
-    foldr f b (FreeMod m) = foldr (\(a,r) b -> f (r .* a) b) b $ Map.toList m
-    foldl f b (FreeMod m) = foldl (\b (a,r) -> f b (r .* a)) b $ Map.toList m
-    foldl' f b (FreeMod m) = foldl' (\b (a,r) -> f b (r .* a)) b $ Map.toList m
-    
-    foldr1 f (FreeMod m) = foldr1 f $ map (\(a,r) -> r.*a) $ Map.toList m
-    foldl1 f (FreeMod m) = foldl1 f $ map (\(a,r) -> r.*a) $ Map.toList m
-
-instance (Num r, Ord a) => Abelian (FreeMod r a)
-instance (Num r, Ord a) => Monoid (FreeMod r a) where
-    mempty = FreeMod mempty
-    mappend = (<>)
-    
-instance (Num r, Ord a) => Group (FreeMod r a) where
-    inverse (FreeMod m) = FreeMod $ Map.map negate m
-
-instance (Num r, Ord a) => LeftModule r (FreeMod r a)
-instance (Num r, Ord a) => LeftOperator r (FreeMod r a) where
-    r .* (FreeMod m) = FreeMod $ Map.map (r*) m
-    
-instance (Num r, Ord a) => RightModule r (FreeMod r a)
-instance (Num r, Ord a) => RightOperator r (FreeMod r a) where
-    a *. r = r .* a
-
-list2module :: (Num r, Ord r, Ord a) => [a] -> FreeMod r a
-list2module xs = FreeMod $ Map.fromList $ go 0 (head sorted) [] sorted
-    where
-        sorted = sort xs
-        
-        go n x retL [] = (x,n):retL
-        go n x retL xs = if (head xs) == x
-            then go (n+1) x retL (tail xs)
-            else go 1 (head xs) ((x,n):retL) (tail xs)
-
+instance (Module m, Fractional (Ring m)) => VectorSpace m
