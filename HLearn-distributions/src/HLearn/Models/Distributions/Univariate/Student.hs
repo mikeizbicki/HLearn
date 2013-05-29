4a07b55914688bfb26580eebe33d25fcb2fe7e7d
n <npezolano@gmail.com>
2013-05-29 00:27:28 -0400

    fixed and modifed student t

--- HLearn-distributions/src/HLearn/Models/Distributions/Univariate/Student.hs
+++ HLearn-distributions/src/HLearn/Models/Distributions/Univariate/Student.hs
@@ -30,39 +30,32 @@ import HLearn.Models.Distributions.Univariate.Internal.Moments
 import HLearn.Models.Distributions.Visualization.Gnuplot
 
 -------------------------------------------------------------------------------
--- data types
-{-- <TODO>
+-- data type
+
 newtype Student prob = Student (Moments3 prob)
     deriving (Read,Show,Eq,Ord,Monoid,Group,Abelian,Module,NumDP,NFData)
---}
+
 -------------------------------------------------------------------------------
 -- training
-{-- <TODO>
-instance (Num prob) => HomTrainer (Student prob) where
+
+instance (Num prob) => HTrainer (Student prob) where
     type Datapoint (Student prob) = prob
     train1dp dp = Student $ train1dp dp
 
 instance (Num prob) => HasRing (Student prob) where
     type Ring (Student prob) = prob
---}
+
 -------------------------------------------------------------------------------
 -- algebra
 
 instance (Num prob) => Probabilistic (Student prob) where
     type Probability (Student prob) = prob
 
-{-- there are more efficent ways to do this --}
 instance (Floating prob) => PDF (Student prob) where
-    pdf dist dp mu sigma df = ( gamma ( (df + 1) / 2)  / sqrt (df * pi) * gamma ( df/2) ) *  ( 1 + (x*x / df) )**( (-1) * ((df + 1) /2)) 
+    pdf dist dp mu sigma2 df = ( gamma ( (df + 1) / 2) / gamma (df/2) ) * (1/(sqrt (df * pi * sigma2) ))*  ( 1 + ( 1/df ) * (( dp - mu)**2 / sigma2 ) )**( (-1) * ((df + 1) /2))
         where
-            sigma2 = variance dist
-            mu = mean dist
-
-instance (Fractional prob) => Mean (Student prob) where
-    mean (Student dist) = m1 dist / m0 dist
-
-instance (Fractional prob) => Variance (Student prob) where
-    variance Student@(Student dist) = m2 dist / m0 dist - (mean Student)*(mean Student)
+            variance = sigma2 * (df / (df - 2) )
+            mean = mu
 
 instance 
     ( Floating prob
