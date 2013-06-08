3782bdbb0cdb246d0ba79027a1726e0a1e3891e1
Mike Izbicki <mike@izbicki.me>
2013-06-07 20:29:12 -0700

    Functor for KDE

--- HLearn-distributions/src/HLearn/Models/Distributions/Univariate/KernelDensityEstimator.hs
+++ HLearn-distributions/src/HLearn/Models/Distributions/Univariate/KernelDensityEstimator.hs
@@ -6,6 +6,8 @@ import Control.DeepSeq
 import qualified Data.Map as Map
 import GHC.TypeLits
 
+import qualified Control.ConstraintKinds as CK
+
 import HLearn.Algebra
 import HLearn.Models.Distributions.Common
 import HLearn.Models.Distributions.Kernels
@@ -15,7 +17,7 @@ import HLearn.Models.Distributions.Kernels
 
 -- | The KDE type is implemented as an isomorphism with the FreeModule
 newtype KDE kernel (h::Nat) prob = KDE
-    { freemod :: FreeModule prob prob
+    { freemod :: FreeModule Int prob
     }
     deriving (Read,Show,Eq,Ord,NFData,Monoid,Group,Abelian,Module)
 
@@ -26,12 +28,18 @@ instance (Num prob) => HasRing (KDE kernel h prob) where
     type Ring (KDE kernel h prob) = prob
     
 instance (Num prob) => NumDP (KDE kernel h prob) where
-    numdp kde = Map.foldr (+) 0 $ getMap $ freemod kde
+    numdp kde = fromIntegral $ Map.foldr (+) 0 $ getMap $ freemod kde
     
 instance (Num prob, Ord prob) => HomTrainer (KDE kernel h prob) where
     type Datapoint (KDE kernel h prob) = prob
     train1dp dp = KDE $ FreeModule $ Map.singleton dp 1
-    
+
+---------------------------------------
+
+instance CK.Functor (KDE kernel h) where
+    type FunctorConstraint (KDE kernel h) prob = Ord prob
+    fmap f = KDE . CK.fmap f . freemod
+
 -------------------------------------------------------------------------------
 -- Distribution
     
@@ -44,4 +52,4 @@ instance (Kernel kernel prob, SingI h, Fractional prob) => PDF (KDE kernel h pro
             f = evalKernel (undefined::kernel)
             n = numdp kde
             h = fromIntegral $ fromSing (sing :: Sing h)
-            dpList = Map.elems (getMap $ freemod kde)
+            dpList = Map.keys (getMap $ freemod kde)
