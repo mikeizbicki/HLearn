ed664bbcd79c486d1c882128f4dd2c4d72a6a415
Mike Izbicki <mike@izbicki.me>
2013-06-07 19:11:06 -0700

    Added CK.Functor to FreeModule

--- HLearn-algebra/src/HLearn/Algebra/Structures/Free/FreeModule.hs
+++ HLearn-algebra/src/HLearn/Algebra/Structures/Free/FreeModule.hs
@@ -1,4 +1,3 @@
-
 module HLearn.Algebra.Structures.Free.FreeModule
     ( FreeModule (..)
     , list2module
@@ -10,28 +9,29 @@ import qualified Control.ConstraintKinds as CK
 import Control.DeepSeq
 import Data.List
 import qualified Data.Map as Map
+
+import HLearn.Algebra.Models.HomTrainer
 import HLearn.Algebra.Structures.Groups
 import HLearn.Algebra.Structures.Modules
 
 -------------------------------------------------------------------------------
--- FreeModuleule
+-- data types
 
 newtype FreeModule r a = FreeModule { getMap :: Map.Map a r }
     deriving (Read,Show,Eq,Ord,NFData)
 
--- instance CK.Functor (FreeModule r) where
---     type FunctorConstraint (FreeModule r) a = (Num r, Ord a)
---     fmap f (FreeModule m) = FreeModule $ Map.mapKeysWith (+) f m
--- 
--- instance CK.Foldable (FreeModule r) where
---     type FoldableConstraint (FreeModule r) a = (Num r, Ord a{-, Operator r a-})
--- --     foldr f b (FreeModule m) = Map.foldrWithKey (\a r b -> f (r .* a) b) b m
---     foldr f b (FreeModule m) = foldr (\(a,r) b -> f (r .* a) b) b $ Map.toList m
---     foldl f b (FreeModule m) = foldl (\b (a,r) -> f b (r .* a)) b $ Map.toList m
---     foldl' f b (FreeModule m) = foldl' (\b (a,r) -> f b (r .* a)) b $ Map.toList m
---     
---     foldr1 f (FreeModule m) = foldr1 f $ map (\(a,r) -> r.*a) $ Map.toList m
---     foldl1 f (FreeModule m) = foldl1 f $ map (\(a,r) -> r.*a) $ Map.toList m
+list2module :: (Num r, Ord r, Ord a) => [a] -> FreeModule r a
+list2module xs = FreeModule $ Map.fromList $ go 0 (head sorted) [] sorted
+    where
+        sorted = sort xs
+        
+        go n x retL [] = (x,n):retL
+        go n x retL xs = if (head xs) == x
+            then go (n+1) x retL (tail xs)
+            else go 1 (head xs) ((x,n):retL) (tail xs)
+            
+-------------------------------------------------------------------------------
+-- algebra
 
 instance (Num r, Ord a) => Abelian (FreeModule r a)
 instance (Num r, Ord a) => Monoid (FreeModule r a) where
@@ -47,13 +47,15 @@ instance (Num r) => HasRing (FreeModule r a) where
 instance (Num r, Ord a) => Module (FreeModule r a) where
     r .* (FreeModule m) = FreeModule $ Map.map (r*) m
 
-list2module :: (Num r, Ord r, Ord a) => [a] -> FreeModule r a
-list2module xs = FreeModule $ Map.fromList $ go 0 (head sorted) [] sorted
-    where
-        sorted = sort xs
-        
-        go n x retL [] = (x,n):retL
-        go n x retL xs = if (head xs) == x
-            then go (n+1) x retL (tail xs)
-            else go 1 (head xs) ((x,n):retL) (tail xs)
+---------------------------------------
 
+instance CK.Functor (FreeModule r) where
+    type FunctorConstraint (FreeModule r) x = Ord x
+    fmap f fm = FreeModule $ Map.mapKeys f $ getMap fm
+
+-------------------------------------------------------------------------------
+-- training
+
+instance (Num r, Ord a) => HomTrainer (FreeModule r a) where
+    type Datapoint (FreeModule r a) = a
+    train1dp dp = FreeModule $ Map.singleton dp 1 
