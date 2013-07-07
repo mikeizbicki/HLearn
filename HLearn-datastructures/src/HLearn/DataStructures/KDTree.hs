3243915398c4d60416fd043d5ca2d72cd657c8fd
Mike Izbicki <mike@izbicki.me>
2013-07-07 02:08:20 -0700

    The KDTree is valid, but not good

--- HLearn-datastructures/src/HLearn/DataStructures/KDTree.hs
+++ HLearn-datastructures/src/HLearn/DataStructures/KDTree.hs
@@ -18,6 +18,12 @@ data KDTree
 
 ---------------------------------------
 
+ppshow :: KDTree -> String
+ppshow = go 0
+    where
+        go i Leaf = ""
+        go i t = replicate i ' ' ++ (show $ val t) ++ "\n" ++ go (i+1) (left t) ++ go (i+1) (right t)
+
 (!) :: DP -> Dimension -> Double
 (!) = (V.!)
 
@@ -32,9 +38,9 @@ insert t dp
     | dp ! splitdim t > val t ! splitdim t = t { right = insert (right t) dp }
     | dp ! splitdim t < val t ! splitdim t = t { left = insert (left t) dp }
 
-validKDTree :: KDTree -> Bool
-validKDTree Leaf = True
-validKDTree t    = thisvalid && validKDTree (left t) && validKDTree (right t)
+isValid :: KDTree -> Bool
+isValid Leaf = True
+isValid t    = thisvalid && isValid (left t) && isValid (right t)
     where
         thisvalid = go (left t) (\dp -> dp ! splitdim t < val t ! splitdim t)
                  && go (right t) (\dp -> dp ! splitdim t > val t ! splitdim t)
@@ -50,20 +56,19 @@ instance Monoid KDTree where
     mappend Leaf Leaf = Leaf
     mappend Leaf a = a
     mappend a Leaf = a
-    mappend a b
-        | splitdim a == splitdim b && val a ! splitdim a < val b ! splitdim b = Node 
-            { val = val b
-            , splitdim = splitdim b
-            , left = a <> left b
-            , right = right b
-            }
-        | splitdim a == splitdim b && val a ! splitdim a > val b ! splitdim b = Node
-            { val = val b
-            , splitdim = splitdim b
-            , left = left b
-            , right = a <> right b
-            }
-        | otherwise = error $ "KDTree.mappend: a=" ++ show a ++ ", b="++show b
+    mappend a b = if
+        | splitdim a == splitdim b -> if
+            | val a ! splitdim a <  val b ! splitdim b -> b 
+                { left = left a <> left b <> train1dp (val a) 
+                } <> right a 
+            | val a ! splitdim a >  val b ! splitdim b -> b 
+                { right = right a <> right b <> train1dp (val a)
+                } <> left a
+            | val a ! splitdim a == val b ! splitdim b -> b 
+                { left = left a <> left b <> train1dp (val a)
+                , right = right a <> right b 
+                } 
+        | otherwise -> error $ "KDTree.mappend: a=" ++ show a ++ ", b="++show b
 
 ---------------------------------------
 
@@ -79,6 +84,8 @@ instance HomTrainer KDTree where
 -------------------------------------------------------------------------------
 -- testing
 
-xs = map V.fromList [[1,5],[-1,4],[0,2],[2,-1],[-2,3],[-3,1]]
-m = train xs :: KDTree
-m' = foldl insert Leaf xs
+ds1 = map V.fromList [[1,5],[-1,4],[0,2],[2,-1],[-2,3],[-3,1]]
+ds2 = map V.fromList [[-1,5],[1,4],[1,2],[-2,-1],[-3,3],[-3,1]]
+m1 = train ds1 :: KDTree
+m2 = train ds2 :: KDTree
+m' = foldl insert Leaf ds1
