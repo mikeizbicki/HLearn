ed9175e90bdb387f0fd277326279bc748fef87eb
Mike Izbicki <mike@izbicki.me>
2013-07-13 15:40:15 -0700

    pruning works, but still kind of slow

---
+++ HLearn-datastructures/src/dev-test/Criterion-KDTree.hs
@@ -0,0 +1,20 @@
+
+import Criterion.Config
+import Criterion.Main
+import qualified Data.Vector as V
+
+import HLearn.Algebra
+import HLearn.DataStructures.KDTree
+
+myConfig = defaultConfig 
+    { cfgPerformGC = ljust True
+    , cfgSamples = ljust 20 
+    }
+
+main = do
+    m <- randmodel
+    print $  "depth randmodel="++show (depth m)
+    defaultMainWith myConfig (return ())
+        [ bench "mindist" $ nf (mindist $ V.fromList [0,0]) m 
+        , bench "mindist_noprune" $ nf (mindist_noprune $ V.fromList [0,0]) m 
+        ]
