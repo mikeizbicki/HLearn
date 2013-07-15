2807d2bca21c3ceb69ad0a894b0007af295d89b6
Mike Izbicki <mike@izbicki.me>
2013-07-14 23:56:58 -0700

    Slightly cleaned indexing and pruning

--- HLearn-datastructures/src/HLearn/DataStructures/KDTree.hs
+++ HLearn-datastructures/src/HLearn/DataStructures/KDTree.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module HLearn.DataStructures.KDTree
     where
     
@@ -6,32 +8,34 @@ import Control.Monad.Random
 import qualified Data.Vector as V
 import Test.QuickCheck
 import Debug.Trace
+import Data.Ix
 
 import qualified Data.Foldable as F
 import qualified Data.Traversable as T
 
-import HLearn.Algebra
+import Data.DependentIndexing
+import HLearn.Algebra hiding (Index)
 
 -------------------------------------------------------------------------------
 -- data types
 
-class 
-    ( Num (DimensionIndex x)
-    , Bounded (DimensionIndex x)
-    , Ord (DimensionIndex x)
-    , Ord (DimensionBase x)
-    ) => HasDimensions x 
-        where
-    type DimensionIndex x 
-    type DimensionBase x
-    (!) :: x -> DimensionIndex x -> DimensionBase x
-
 data KDTree dp
     = Leaf 
-    | Node { val :: dp, splitdim :: DimensionIndex dp, left :: KDTree dp, right :: KDTree dp}
+    | Node { val :: dp, splitdim :: IndexType dp, left :: KDTree dp, right :: KDTree dp}
 
-deriving instance (Show dp, Show (DimensionIndex dp)) => Show (KDTree dp)
-deriving instance (Read dp, Read (DimensionIndex dp)) => Read (KDTree dp)
+data TreeIndex = TreeLeft | TreeRight
+    deriving (Read,Show,Eq,Ord,Bounded,Enum)
+
+instance (Ord (IndexType dp), Ord dp) => Index (KDTree dp) where
+    type IndexType (KDTree dp) = TreeIndex
+    type IndexResult (KDTree dp) = KDTree dp
+    (!) t TreeLeft  = left t
+    (!) t TreeRight = right t
+
+deriving instance (Show dp, Show (IndexType dp)) => Show (KDTree dp)
+deriving instance (Read dp, Read (IndexType dp)) => Read (KDTree dp)
+deriving instance (Eq dp, Eq (IndexType dp)) => Eq (KDTree dp)
+deriving instance (Ord dp, Ord (IndexType dp)) => Ord (KDTree dp)
 
 ---------------------------------------
 
@@ -41,7 +45,7 @@ ppshow = go []
         go i Leaf = "" -- i++"*\n"
         go i t = i ++ " " ++ (show $ val t) ++ "\n" ++ go (i++"l") (left t) ++ go (i++"r") (right t)
 
-basicshow :: (Show dp, Show (DimensionIndex dp)) => KDTree dp -> String
+basicshow :: (Show dp, Show (IndexType dp)) => KDTree dp -> String
 basicshow Leaf = "Leaf"
 basicshow t = "Node { val="++show (val t)++", splitdim="++show (splitdim t)++", left="++go (left t)++", right="++go (right t)++" }"
     where
@@ -49,18 +53,18 @@ basicshow t = "Node { val="++show (val t)++", splitdim="++show (splitdim t)++", 
         go _ = "Node"
 
 
-nextSplitDim :: (HasDimensions dp) => KDTree dp -> DimensionIndex dp
+nextSplitDim :: (Index dp) => KDTree dp -> IndexType dp
 nextSplitDim t 
     | splitdim t == maxBound = minBound
-    | otherwise = splitdim t+1
+    | otherwise = succ $ splitdim t
 
-insert :: (HasDimensions dp) => KDTree dp -> dp -> KDTree dp
-insert Leaf dp = Node dp 0 Leaf Leaf
+insert :: (Index dp) => KDTree dp -> dp -> KDTree dp
+insert Leaf dp = Node dp minBound Leaf Leaf
 insert t dp
     | dp ! splitdim t >  val t ! splitdim t = t { right = insert (right t) dp }
     | dp ! splitdim t <= val t ! splitdim t = t { left = insert (left t) dp }
 
-isValid :: (HasDimensions dp) => KDTree dp -> Bool
+isValid :: (Index dp) => KDTree dp -> Bool
 isValid Leaf = True
 isValid t    = thisvalid && isValid (left t) && isValid (right t)
     where
@@ -115,7 +119,7 @@ debug _ _ _ action = action
 --     )
 --     action
 
-instance (HasDimensions dp) => Monoid (KDTree dp) where
+instance (Index dp) => Monoid (KDTree dp) where
     mempty = Leaf
     mappend Leaf Leaf = Leaf
     mappend Leaf a = a
@@ -149,26 +153,27 @@ instance F.Foldable KDTree where
     foldr f i Leaf = i
     foldr f i t = F.foldr f (F.foldr f (f (val t) i) (right t)) (left t)
 
-class Prunable t selector | t -> selector where
-    pfoldr :: (b -> t a ->             Bool) -> (a -> b -> b) -> b -> t a -> b
-    qfoldr :: (b -> t a -> selector -> Bool) -> (a -> b -> b) -> b -> t a -> b
+class Prunable t where
+    pfoldr :: (b -> t a ->                    Bool) -> (a -> b -> b) -> b -> t a -> b
+    qfoldr :: (b -> t a -> IndexType (t a) -> Bool) -> (a -> b -> b) -> b -> t a -> b
 
-data Selector_KDTree = TH_Right | TH_Left
-
-instance Prunable KDTree Selector_KDTree where
+instance Prunable KDTree where
     pfoldr p f i Leaf = i
     pfoldr p f i t = if p i t 
         then pfoldr p f (pfoldr p f (f (val t) i) (right t)) (left t) 
         else i
 
     qfoldr p f i Leaf = i
-    qfoldr p f i t = if p i t TH_Left
+    qfoldr p f i t = if p i t TreeLeft
         then qfoldr p f dorightfold (left t)
-        else dorightfold
+        else trace ("TreeLeft False" ++ show (size $ left t)) $ dorightfold
         where
-            dorightfold = if p i t TH_Right
+            dorightfold = if p i t TreeRight
                 then qfoldr p f (f (val t) i) (right t)
-                else i
+                else trace ("TreeRight False "++show (size $ right t)
+    --                 ++ "\n  i:"++show i
+    --                 ++ "\n  t:"++basicshow t
+                    ) $ i
 
 class (F.Foldable t) => DualFoldable t where
     dfoldr :: ((a,a) -> b -> b) -> b -> t a -> t a -> b
@@ -187,23 +192,33 @@ nn_score :: Maybe dp -> KDTree dp -> Bool
 nn_score Nothing t = True
 nn_score (Just query) t = True
 
+mindist_prune query (dist,dp) t TreeLeft  = (query ! splitdim t) - (val t ! splitdim t) < dist 
+mindist_prune query (dist,dp) t TreeRight = (val t ! splitdim t) - (query ! splitdim t) < dist
+
+mindist_cata query a (dist,dp) = if dist > distance query a
+    then (distance query a, a)
+    else (dist,dp)
+
+mindist_prune_count query (count,(dist,dp)) t TreeLeft  = (query ! splitdim t) - (val t ! splitdim t) < dist 
+mindist_prune_count query (count,(dist,dp)) t TreeRight = (val t ! splitdim t) - (query ! splitdim t) < dist
+
+count_cata _ x = x+1
+count_cata' _ (x::Int,t) = (x+1,t)
+
+noprune _ _ _ = True
+
+q = (0,0) :: (Double,Double)
+
 mindist :: 
-    ( HasDimensions dp
-    , DimensionBase dp ~ Ring dp
+    ( Index dp
+    , IndexResult dp ~ Ring dp
     , MetricSpace dp
     , Ord (Ring dp)
-    , Show dp, Show (Ring dp), Show (DimensionIndex dp)
+    , Show dp, Show (Ring dp), Show (IndexType dp)
     ) => dp -> KDTree dp -> (Ring dp, dp)
 mindist query Leaf = error "KDTree.mindist on Leaf"
-mindist query t = qfoldr prune cata start t
+mindist query t = qfoldr (mindist_prune query) (mindist_cata query) start t
     where
-        prune (dist,dp) t TH_Left  = (query ! splitdim t) - (val t ! splitdim t) < dist 
-        prune (dist,dp) t TH_Right = (val t ! splitdim t) - (query ! splitdim t) < dist
-
-        cata a (dist,dp) = if dist > distance query a
-            then (distance query a, a)
-            else (dist,dp)
-
         start = (distance query (val t), val t) 
 
 mindist_noprune :: 
@@ -244,9 +259,9 @@ nn_noprune query t = F.foldr (nn_basecase query) Nothing t
 -------------------------------------------------------------------------------
 -- model
 
-instance (HasDimensions dp) => HomTrainer (KDTree dp) where
+instance (Index dp) => HomTrainer (KDTree dp) where
     type Datapoint (KDTree dp) = dp
-    train1dp dp = Node dp 0 Leaf Leaf
+    train1dp dp = Node dp minBound Leaf Leaf
 
 -------------------------------------------------------------------------------
 -- testing
@@ -257,43 +272,10 @@ instance Arbitrary (Double,Double) where
         b <- choose (-100,100)
         return (a,b) 
 
-instance (HasDimensions dp, Arbitrary dp) => Arbitrary (KDTree dp) where
+instance (Index dp, Arbitrary dp) => Arbitrary (KDTree dp) where
     arbitrary = do
         xs <- arbitrary
         return $ train (xs :: [Datapoint (KDTree dp)])
-
-class DependentIndex datatype index result | datatype index -> result where
-    (#) :: datatype -> index -> result
-
-data TI_1 = TI_1
-data TI_2 = TI_2
-data TI_3 = TI_3
-
-class Index datatype index result | datatype -> index result where
-    (##) :: datatype -> index -> result
-
--- instance Index (a,a) Index_Tuple2 a where
---     (##) (a0,a1) 0 = a0
---     (##) (a0,a1) 1 = a1
-
-data Index_Tuple2 = Index_Tuple2_1 | Index_Tuple2_2
-
-instance DependentIndex (a,b) TI_1 a where (a,b) # TI_1 = a
-instance DependentIndex (a,b) TI_2 b where (a,b) # TI_2 = b
-
-class CompareTypeLens base index where
-    compareTL :: base -> base -> index -> Bool 
-
-instance (Ord a, Ord b) => CompareTypeLens (a,b) Index_Tuple2 where
-    compareTL b1 b2 Index_Tuple2_1 = b1 # TI_1 < b2 # TI_1
-    compareTL b1 b2 Index_Tuple2_2 = b1 # TI_2 < b2 # TI_2
-
-instance (Ord a) => HasDimensions (a,a) where
-    type DimensionIndex (a,a) = Int
-    type DimensionBase (a,a) = a
-
-    (!) (a0,a1) 0 = a0
-    (!) (a0,a1) 1 = a1
 
 instance (Num a) => HasRing (a,a) where
     type Ring (a,a) = a
@@ -301,12 +283,8 @@ instance (Num a) => HasRing (a,a) where
 instance (Floating a) => MetricSpace (a,a) where
     distance (x1,y1) (x2,y2) = sqrt $ (x1-x2)^2 + (y1-y2)^2
 
-instance DependentIndex (a,b,c) TI_1 a where (a,b,c) # TI_1 = a
-instance DependentIndex (a,b,c) TI_2 b where (a,b,c) # TI_2 = b
-instance DependentIndex (a,b,c) TI_3 c where (a,b,c) # TI_3 = c
-
 randmodel len = fmap (train) $ replicateM len $ do
-    x <- randomIO
-    y <- randomIO
+    x <- randomRIO (-1000,1000)
+    y <- randomRIO (-1000,1000)
     return (x,y) 
     :: IO (KDTree (Double,Double))
