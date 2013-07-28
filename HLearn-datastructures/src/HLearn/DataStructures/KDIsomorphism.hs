06ca9b0e77cccf35feaf38be234f7c9b71896143
Mike Izbicki <mike@izbicki.me>
2013-07-28 01:22:57 -0700

    bitzip/bitunzip work on binary sized inputs

--- HLearn-datastructures/src/HLearn/DataStructures/KDIsomorphism.hs
+++ HLearn-datastructures/src/HLearn/DataStructures/KDIsomorphism.hs
@@ -13,12 +13,86 @@ import Test.QuickCheck hiding ((.&.),(.|.))
 -------------------------------------------------------------------------------
 -- Bitwise
 
+-- newtype KDBitwise = KDBitwise ByteArray
+-- 
+-- mkKDBitwise :: [Bitwise] -> KDBitwise
+-- mkKDBitwise xs = KDBitwise $ runST $ do
+--     arr <- newByteArray $ 8 * length xs
+--     unsafeFreezeByteArray arr
+--     where
+--         insertBitwise x offset arr = do
+
+x=toBitwise (maxBound :: Word64)
+y=toBitwise (minBound :: Word64)
+xx = bitzip x x
+xy = bitzip x y
+yx = bitzip y x
+yy = bitzip y y
+xxxx = bitzip xx xx
+
+-- | performs a "bitwise zip" on two "Bitwise".  They must contain an even number of "Word64"s, and the same amount.
+bitzip :: Bitwise -> Bitwise -> Bitwise
+bitzip (Bitwise arr1) (Bitwise arr2) = Bitwise $ runST $ do
+    ret <- newByteArray (sizeofByteArray arr1 *2)
+    forM [0..floor $ (fromIntegral $ sizeofByteArray arr1 :: Rational) /8-1]  (\i -> do 
+        writeByteArray ret (2*i)   $ word1 i
+        writeByteArray ret (2*i+1) $ word2 i
+        )
+    unsafeFreezeByteArray ret
+    where
+        arr1word i = indexByteArray arr1 i :: Word64
+        arr2word i = indexByteArray arr2 i :: Word64
+
+        word1 i = word1arr1 i .|. word1arr2 i
+        word1arr1 i = foldr1 (.|.) $ map (go (arr1word i) 0) [0..31] 
+        word1arr2 i = foldr1 (.|.) $ map (go (arr2word i) 1) [0..31] 
+
+        word2 i = word2arr1 i .|. word2arr2 i
+        word2arr1 i = foldr1 (.|.) $ map (go (arr1word i) (-64)) [32..63] 
+        word2arr2 i = foldr1 (.|.) $ map (go (arr2word i) (-63)) [32..63] 
+
+        go :: Word64 -> Int -> Int -> Word64
+        go x offset i = if testBit x i
+            then bit $ i*2+offset
+            else 0
+
+-- | performs a "bitwise unzip" on a "Bitwise".  It must contain an even number of "Word64"s.
+bitunzip :: Bitwise -> (Bitwise,Bitwise)
+bitunzip (Bitwise arr) = runST $ do
+    ret1 <- newByteArray (ceiling $ (fromIntegral $ sizeofByteArray arr :: Rational) / 2)
+    ret2 <- newByteArray (ceiling $ (fromIntegral $ sizeofByteArray arr :: Rational) / 2)
+    forM [0..floor $ (fromIntegral $ sizeofByteArray arr :: Rational) /16-1]  (\i -> do 
+        writeByteArray ret1 i $ word1 i
+        writeByteArray ret2 i $ word2 i
+        )
+    ret1 <- unsafeFreezeByteArray ret1
+    ret2 <- unsafeFreezeByteArray ret2
+    return (Bitwise ret1, Bitwise ret2)
+    where
+        arr1word i = indexByteArray arr (i*2)   :: Word64
+        arr2word i = indexByteArray arr (i*2+1) :: Word64
+
+        word1 i = word1arr1 i .|. word1arr2 i
+        word1arr1 i = foldr1 (.|.) $ map (go (arr1word i) 0 0) [0..31]
+        word1arr2 i = foldr1 (.|.) $ map (go (arr2word i) 0 32) [0..31]
+
+        word2 i = word2arr1 i .|. word2arr2 i
+        word2arr1 i = foldr1 (.|.) $ map (go (arr1word i) 1 0) [0..31]
+        word2arr2 i = foldr1 (.|.) $ map (go (arr2word i) 1 32) [0..31]
+
+        go :: Word64 -> Int -> Int -> Int-> Word64
+        go x arrnum offset i = if testBit x (i*2+arrnum)
+            then bit $ i+offset
+            else 0
+
+---------------------------------------
+
 newtype Bitwise = Bitwise ByteArray
 
-instance Eq Bitwise where
+instance Eq (Bitwise ) where
     bw1 == bw2 = compare bw1 bw2 == EQ
 
-instance Ord Bitwise where
+instance Ord (Bitwise) where
     compare (Bitwise arr1) (Bitwise arr2) = go 0
         where
             go i = case compare x1 x2 of
@@ -34,7 +108,7 @@ instance Ord Bitwise where
 ---------------------------------------
 -- show
 
-instance Show Bitwise where
+instance Show (Bitwise) where
     show = showBitwise word8bits
     
 showBitwise f = \ (Bitwise arr) -> concat $ map ((' ':) . f . indexByteArray arr) [0..sizeofByteArray arr-1] 
@@ -74,7 +148,7 @@ word8bits x = concat $ map go [0..7]
 ---------------------------------------
 -- read 
 
-string2bitwise :: String -> Bitwise
+string2bitwise :: String -> Bitwise 
 string2bitwise xs = Bitwise $ runST $ do 
     arr <- newByteArray (length bitList)
     mapM_ (\(i,x) -> writeByteArray arr i x) $ zip [0..] bitList 
