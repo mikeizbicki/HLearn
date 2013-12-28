77cf9af7f12f577978aa26dc762e88bb5a6e08a3
Mike Izbicki <mike@izbicki.me>
2013-12-27 16:49:35 -0800

    Linear regression now has clean type level syntax

--- src/HLearn/Algebra/Types/Frac.hs
+++ src/HLearn/Algebra/Types/Frac.hs
@@ -6,29 +6,24 @@ import GHC.TypeLits
 
 ---------------------------------------
 
-data Frac = (/) Nat Nat 
+data Frac = Frac Nat Nat 
 
-data instance Sing (n::Frac) = SFloat Integer Integer
+data instance Sing (n::Frac) = SFrac Integer Integer
 
-instance (SingI a, SingI b) => SingI ((/) a b) where
-    sing = SFloat (fromSing (sing :: Sing a)) (fromSing (sing :: Sing b))
+instance (SingI a, SingI b) => SingI ('Frac a b) where
+    sing = SFrac (fromSing (sing :: Sing a)) (fromSing (sing :: Sing b))
 
 instance Fractional r => SingE (Kind :: Frac) r where
-    fromSing (SFloat a b) = fromIntegral a/fromIntegral b
+    fromSing (SFrac a b) = fromIntegral a/fromIntegral b
 
----------------------------------------
+type (/) a b = 'Frac a b
 
--- type family (#) (a::Nat) (b::Nat) :: Frac
--- type instance (#) a b = a:.b
--- 
--- data Frac = (:.) Nat Nat
+-- data Frac = (/) Nat Nat 
 -- 
 -- data instance Sing (n::Frac) = SFrac Integer Integer
 -- 
--- instance (SingI a, SingI b) => SingI (a:.b) where
---     sing = SFrac (fromSing (sing::Sing a)) (fromSing (sing::Sing b))
+-- instance (SingI a, SingI b) => SingI ((/) a b) where
+--     sing = SFrac (fromSing (sing :: Sing a)) (fromSing (sing :: Sing b))
 -- 
 -- instance Fractional r => SingE (Kind :: Frac) r where
---     fromSing (SFrac a b) = fromIntegral a + fromIntegral b/(10^^numdigits b)
---         where
---             numdigits b = fromIntegral $ length $ show b
+--     fromSing (SFrac a b) = fromIntegral a/fromIntegral b
