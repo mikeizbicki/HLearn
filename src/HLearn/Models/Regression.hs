3445c2747308086f710ff2b95464551b52ff8008
Mike Izbicki <mike@izbicki.me>
2013-12-30 21:10:55 -0800

    Added pretty printer for Regression

--- src/HLearn/Models/Regression.hs
+++ src/HLearn/Models/Regression.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds,PolyKinds,QuasiQuotes #-}
+{-# LANGUAGE DataKinds,PolyKinds,QuasiQuotes,RankNTypes,MultiParamTypeClasses,FlexibleContexts,UndecidableInstances #-}
 
 module HLearn.Models.Regression
     where
@@ -6,6 +6,7 @@ module HLearn.Models.Regression
 import Data.List
 import qualified Data.Foldable as F
 import qualified Data.Semigroup as SG
+import Numeric
 
 import Foreign.Storable
 import Numeric.LinearAlgebra hiding ((<>))
@@ -18,9 +19,23 @@ import HLearn.Models.Regression.Parsing
 -------------------------------------------------------------------------------
 -- data types
 
-type Regression eq dp = AddUnit1 (Reg eq) dp
+newtype Regression expr dp = Regression { unRegression :: AddUnit1 (Reg expr) dp }
 
-data Reg (eq:: [TermT]) dp = Reg 
+deriving instance (Element (Ring dp), Field (Ring dp), Container Vector (Ring dp)) => Monoid (Regression expr dp)
+
+instance (Show (Ring dp), RealFloat (Ring dp), Element (Ring dp), SingI expr) => Show (Regression expr dp) where
+--     show m = show $ fromSing (sing :: Sing expr)
+    show m = "y = " ++ (concat $ intersperse " + " $ map prettyPrint $ filter shouldPrint $ zip coefL termL)
+        where
+            shouldPrint (c,t) = abs c > 0.000001
+            
+            prettyPrint (c,CCon t) = showGFloat (Just 4) (c * fromRational t) ""
+            prettyPrint (c,t) = showGFloat (Just 4) c "" ++ ppShowTerm t
+
+            termL = fromSing (sing :: Sing expr)
+            coefL = concat . toLists $ beta $ unUnit1 $ unRegression $ m
+
+data Reg (expr:: [TermT]) dp = Reg 
     { beta :: Matrix (Ring dp)
     , rawM  :: !(Matrix (Ring dp))
     , rawV  :: !(Vector (Ring dp))
@@ -31,7 +46,7 @@ mkReg ::
     ( Field (Ring dp)
     , Element (Ring dp)
     , Num (Ring dp)
-    ) => Ring dp -> Vector (Ring dp) -> Matrix (Ring dp) -> Reg eq dp
+    ) => Ring dp -> Vector (Ring dp) -> Matrix (Ring dp) -> Reg expr dp
 mkReg numdp' rawV' rawM' = Reg
     { beta = (inv $ scale (1/numdp') rawM')  LA.<> (scale (1/numdp') $ asColumn rawV') 
     , rawM = rawM'
@@ -47,7 +62,7 @@ instance
     , Container Vector (Ring dp)
     , Field (Ring dp)
     , Fractional (Ring dp)
-    ) => SG.Semigroup (Reg eq dp) where
+    ) => SG.Semigroup (Reg expr dp) where
     r1 <> r2 = mkReg
         (_numdp r1 + _numdp r2)
         (rawV r1 `LA.add` rawV r2)
@@ -66,42 +81,42 @@ instance
     , LA.Product (Attributes dp)
     , Labeled dp
     , Field (Attributes dp)
-    , SingI eq
-    ) => HomTrainer (AddUnit1 (Reg eq) dp)
+    , SingI expr
+    ) => HomTrainer (Regression expr dp)
         where
-    type Datapoint (AddUnit1 (Reg eq) dp) = dp
+    type Datapoint (Regression expr dp) = dp
     
-    train1dp dp = UnitLift1 $ mkReg
+    train1dp dp = Regression $ UnitLift1 $ mkReg
         (1)
-        (scale (getLabel dp) (dp2vec (sing::Sing eq) $ getAttributes dp))
-        ((asColumn $ dp2vec (sing::Sing eq) $ getAttributes dp) LA.<> (asRow $ dp2vec (sing::Sing eq) $ getAttributes dp))
+        (scale (getLabel dp) (dp2vec (sing::Sing expr) $ getAttributes dp))
+        ((asColumn $ dp2vec (sing::Sing expr) $ getAttributes dp) LA.<> (asRow $ dp2vec (sing::Sing expr) $ getAttributes dp))
 
 {-# INLINE dp2vec #-}
--- dp2vec :: (Num dp, Storable dp, SingI eq) => Sing eq -> dp -> Vector dp
+-- dp2vec :: (Num dp, Storable dp, SingI expr) => Sing expr -> dp -> Vector dp
 -- dp2vec _ dp = LA.fromList [1,dp,dp*dp] 
 
-dp2vec :: forall eq dp.
+dp2vec :: forall expr dp.
     ( Floating dp
     , Storable dp
-    , SingI eq
-    ) => Sing (eq :: [TermT]) -> dp -> Vector dp
-dp2vec _ dp = LA.fromList $ map (flip evalTerm dp) $ fromSing (sing :: Sing eq)
+    , SingI expr
+    ) => Sing (expr :: [TermT]) -> dp -> Vector dp
+dp2vec _ dp = LA.fromList $ map (flip evalTerm dp) $ fromSing (sing :: Sing expr)
 
 -------------------------------------------------------------------------------
 -- model use
 
 -- train xs :: Regression Linear [expr| x + log x + x^2|] Double
 
-reg :: forall dp eq. 
+reg :: forall dp expr. 
     ( Attributes dp ~ Label dp
     , Ring dp ~ Attributes dp
     , HasRing dp
     , Floating (Label dp)
     , LA.Product (Label dp)
     , Storable (Label dp)
-    , SingI eq
-    ) => Attributes dp -> Regression eq dp -> Label dp
-reg dp (UnitLift1 r) = ((asRow $ dp2vec (sing::Sing eq) dp) LA.<> (beta r)) @@> (0,0)
+    , SingI expr
+    ) => Attributes dp -> Regression expr dp -> Label dp
+reg dp (Regression (UnitLift1 r)) = ((asRow $ dp2vec (sing::Sing expr) dp) LA.<> (beta r)) @@> (0,0)
 
 -- testdp = [(1,1),(2,1),(3,1),(6,5),(4,5)]
 -- testdp = [(1,1),(2,2),(3,4)]
