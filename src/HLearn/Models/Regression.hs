3d651373f426244511ab8a7e81d46b9e364fbee6
Mike Izbicki <mike@izbicki.me>
2014-01-01 22:45:28 -0800

    Reflection TH generates Parameter classes

--- src/HLearn/Models/Regression.hs
+++ src/HLearn/Models/Regression.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds,PolyKinds,QuasiQuotes,RankNTypes,MultiParamTypeClasses,FlexibleContexts,UndecidableInstances #-}
+{-# LANGUAGE DataKinds,PolyKinds,QuasiQuotes,RankNTypes,MultiParamTypeClasses,FlexibleContexts,UndecidableInstances,TemplateHaskell #-}
 
 module HLearn.Models.Regression
     where
@@ -19,53 +19,59 @@ import HLearn.Models.Regression.Parsing
 import HLearn.Algebra.Types.Reflection
 import Data.Constraint
 import Data.Reflection
+import qualified Language.Haskell.TH as TH
+import Debug.Trace
 
 -------------------------------------------------------------------------------
 -- data types
 
-newtype Regression expr dp = Regression { unRegression :: AddUnit1 (Reg expr) dp }
+data family ModelParams (m :: *)
+data instance ModelParams (Reg method expr dp) = ModelParams_Reg { mp_expr :: [Term] }
 
----------------------------------------
+newtype Regression method expr dp = Regression { unRegression :: AddUnit1 (Reg method expr) dp }
+newtype Regression2 expr2 dp2 = Regression2 { unRegression2 :: AddUnit1 (Reg Nothing expr2) dp2 }
+
+data Method = Linear | Ridge 
+data instance Sing (s::Method) = SMethod Method
+instance SingI 'Linear where sing = SMethod Linear
+instance SingI 'Ridge where sing = SMethod Ridge
+instance SingE (Kind::Method) Method where   
+    fromSing (SMethod r) = r
+
+type family Set_expr (r:: *) (expr:: [TermT]) :: *
+type instance Set_expr (Regression method exprold dp) expr = Regression method (Just expr) dp
+
+type DefRegression dp = Regression Nothing Nothing dp
+
+data Reg (method::Maybe Method) (expr:: Maybe [TermT]) dp = Reg 
+    { beta :: Matrix (Ring dp)
+    , rawM  :: !(Matrix (Ring dp))
+    , rawV  :: !(Vector (Ring dp))
+    , _numdp :: !(Ring dp)
+    , modelparams :: !(ModelParams (Reg method expr dp))
+    }
+
+mkParams ''Regression
 
 class Param_expr m => ValidParams m
 
-instance Param_expr (Regression expr dp) => ValidParams (Regression expr dp)
+instance Param_expr (Regression method expr dp) => ValidParams (Regression method expr dp)
 
-class Param_expr m where
-    param_expr :: m -> [Term]
-
-instance SingI expr => Param_expr (Regression (Just expr) dp) where
-    param_expr _ = fromSing (sing :: Sing expr)
-
-instance Param_expr (Regression Nothing dp) where
+instance Param_expr (Regression method Nothing dp) where
     param_expr m = mp_expr $ modelparams $ unUnit1 $ unRegression m
 
-instance ReifiableConstraint Param_expr where
-    data Def Param_expr m = Param_expr { param_expr_ :: m -> [Term] }
-    reifiedIns = Sub Dict
+str2expr = SetParam_expr . parseExprSimple
 
-instance SetParam Param_expr (Regression Nothing dp) where
-    data DefParam Param_expr (Regression Nothing dp) = 
-           DefParam_expr { unDefParam_expr :: [Term] }
-    setParam p a = using (Param_expr $ \x -> unDefParam_expr p) a 
+test_Nothing = setParam (str2expr "1 + x^2") $ train [(1,0),(2,1),(3,2)] :: Regression (Just Linear) Nothing (Double,Double)
+test_Just = train [(1,0),(2,1),(3,2)] :: Regression (Just Linear) (Just [expr| 1 + x |]) (Double,Double)
+test_Just2 = train [(1,0),(2,1),(3,2)] :: (DefRegression (Double,Double)) `Set_expr` [expr| 1 + x|]
 
-str2expr = DefParam_expr . parseExprSimple
 
-instance Reifies s (Def Param_expr m) => Param_expr (Lift Param_expr m s) where 
-    param_expr m = param_expr_ (reflect m) (lower m)
+deriving instance (Element (Ring dp), Field (Ring dp), Container Vector (Ring dp), ValidParams (Regression method expr dp)) => Monoid (Regression method expr dp)
 
-data family ModelParams (m :: *)
-data instance ModelParams (Reg expr dp) = ModelParams_Reg { mp_expr :: [Term] }
-
-instance SingI (Maybe a)
-
----------------------------------------
-
-deriving instance (Element (Ring dp), Field (Ring dp), Container Vector (Ring dp), ValidParams (Regression expr dp)) => Monoid (Regression expr dp)
-
-instance (Show (Ring dp), RealFloat (Ring dp), Element (Ring dp)) => Show (Regression expr dp) where 
+instance (Show (Ring dp), RealFloat (Ring dp), Element (Ring dp)) => Show (Regression method expr dp) where 
 --     show _ = "show"
--- instance (Show (Ring dp), RealFloat (Ring dp), Element (Ring dp), ValidParams (Regression expr dp)) => Show (Regression expr dp) where
+-- instance (Show (Ring dp), RealFloat (Ring dp), Element (Ring dp), ValidParams (Regression method expr dp)) => Show (Regression method expr dp) where
     show m = "y = " ++ (concat $ intersperse " + " $ map prettyPrint $ filter shouldPrint $ zip coefL termL)
         where
             param_expr m = mp_expr $ modelparams $ unUnit1 $ unRegression m
@@ -77,26 +83,19 @@ instance (Show (Ring dp), RealFloat (Ring dp), Element (Ring dp)) => Show (Regre
             termL = param_expr m 
             coefL = concat . toLists $ beta $ unUnit1 $ unRegression $ m
 
-data Reg (expr:: Maybe [TermT]) dp = Reg 
-    { beta :: Matrix (Ring dp)
-    , rawM  :: !(Matrix (Ring dp))
-    , rawV  :: !(Vector (Ring dp))
-    , _numdp :: !(Ring dp)
-    , modelparams :: !(ModelParams (Reg expr dp))
-    }
 
-mkReg :: forall expr dp.
+mkReg :: forall method expr dp.
     ( Field (Ring dp)
     , Element (Ring dp)
     , Num (Ring dp)
-    , ValidParams (Regression expr dp)
-    ) => Ring dp -> Vector (Ring dp) -> Matrix (Ring dp) -> Reg expr dp
+    , ValidParams (Regression method expr dp)
+    ) => Ring dp -> Vector (Ring dp) -> Matrix (Ring dp) -> Reg method expr dp
 mkReg numdp' rawV' rawM' = Reg
     { beta = (inv $ scale (1/numdp') rawM')  LA.<> (scale (1/numdp') $ asColumn rawV') 
     , rawM = rawM'
     , rawV = rawV'
     , _numdp = numdp'
-    , modelparams = ModelParams_Reg $ param_expr (error "poop1" :: Regression expr dp)
+    , modelparams = ModelParams_Reg $ param_expr (error "poop1" :: Regression method expr dp)
     }
 
 -------------------------------------------------------------------------------
@@ -107,8 +106,8 @@ instance
     , Container Vector (Ring dp)
     , Field (Ring dp)
     , Fractional (Ring dp)
-    , ValidParams (Regression expr dp)
-    ) => SG.Semigroup (Reg expr dp) where
+    , ValidParams (Regression method expr dp)
+    ) => SG.Semigroup (Reg method expr dp) where
     r1 <> r2 = mkReg
         (_numdp r1 + _numdp r2)
         (rawV r1 `LA.add` rawV r2)
@@ -127,17 +126,17 @@ instance
     , LA.Product (Attributes dp)
     , Labeled dp
     , Field (Attributes dp)
-    , ValidParams (Regression expr dp)
-    ) => HomTrainer (Regression expr dp)
+    , ValidParams (Regression method expr dp)
+    ) => HomTrainer (Regression method expr dp)
         where
-    type Datapoint (Regression expr dp) = dp
+    type Datapoint (Regression method expr dp) = dp
     
     train1dp dp = Regression $ UnitLift1 $ mkReg
         (1)
         (scale (getLabel dp) (dp2vec expr $ getAttributes dp))
         ((asColumn $ dp2vec expr $ getAttributes dp) LA.<> (asRow $ dp2vec expr $ getAttributes dp))
         where
-            expr = param_expr (error "poop2" :: Regression expr dp)
+            expr = param_expr (error "poop2" :: Regression method expr dp)
 
 {-# INLINE dp2vec #-}
 dp2vec :: forall dp.
@@ -158,18 +157,18 @@ dp2vec expr dp = LA.fromList $ map (flip evalTerm dp) expr
 
 -- train xs :: Regression Linear [expr| x + log x + x^2|] Double
 
-reg :: forall dp expr. 
+reg :: forall method expr dp. 
     ( Attributes dp ~ Label dp
     , Ring dp ~ Attributes dp
     , HasRing dp
     , Floating (Label dp)
     , LA.Product (Label dp)
     , Storable (Label dp)
-    , ValidParams (Regression expr dp)
-    ) => Attributes dp -> Regression expr dp -> Label dp
+    , ValidParams (Regression method expr dp)
+    ) => Attributes dp -> Regression method expr dp -> Label dp
 reg dp (Regression (UnitLift1 r)) = ((asRow $ dp2vec expr dp) LA.<> (beta r)) @@> (0,0)
     where
-        expr = param_expr (error "poop3" :: Regression expr dp)
+        expr = param_expr (error "poop3" :: Regression method expr dp)
 
 -- testdp = [(1,1),(2,1),(3,1),(6,5),(4,5)]
 -- testdp = [(1,1),(2,2),(3,4)]
@@ -177,4 +176,4 @@ testdp = [(2,1),(5,2),(10,3),(17,4)]
 -- testm = train testdp :: Regression '[VarT] (Double,Double)
 -- testm = train testdp :: Regression '[ConT (1/1),VarT,BinT Pow (ConT (2/1)) VarT] (Double,Double)
 -- testm = train testdp :: Regression '[ConT (1/1),VarT,BinT Mult VarT VarT] (Double,Double)
-testm = train testdp :: Regression (Just [expr| 1 + x + x^2 + log x |]) (Double,Double)
+-- testm = train testdp :: Regression (Just [expr| 1 + x + x^2 + log x |]) (Double,Double)
