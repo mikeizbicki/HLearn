f7aaf6c6a10f61def51a5a0e6d02ab81b9514cc7
Mike Izbicki <mike@izbicki.me>
2014-12-23 02:25:04 -0800

    fixed minor bug in tests

--- scripts/allknn-comparelibs/runtest.sh
+++ scripts/allknn-comparelibs/runtest.sh
@@ -12,10 +12,10 @@ INPUT="$curdir/$1"
 TIME="/usr/bin/time -f %E "
 
 for cmd in \
-    "$curdir/hlearn-allknn --train-method=traininsert_nosort -r $INPUT -k 1 -n neighbors_hlearn.1.csv --distances-file=distances_hlearn.1.csv +RTS -K1000M -N4" \
-    "$curdir/hlearn-allknn --train-method=traininsert_nosort -r $INPUT -k 1 -n neighbors_hlearn.2.csv --distances-file=distances_hlearn.2.csv +RTS -K1000M -N3" \
-    "$curdir/hlearn-allknn --train-method=traininsert_nosort -r $INPUT -k 1 -n neighbors_hlearn.3.csv --distances-file=distances_hlearn.3.csv +RTS -K1000M -N2" \
-    "$curdir/hlearn-allknn --train-method=traininsert_nosort -r $INPUT -k 1 -n neighbors_hlearn.4.csv --distances-file=distances_hlearn.4.csv +RTS -K1000M -N1" \
+    "$curdir/hlearn-allknn --varshift -r $INPUT -k 1 -n neighbors_hlearn.1.csv --distances-file=distances_hlearn.1.csv +RTS -K1000M -N4" \
+    "$curdir/hlearn-allknn --varshift -r $INPUT -k 1 -n neighbors_hlearn.2.csv --distances-file=distances_hlearn.2.csv +RTS -K1000M -N3" \
+    "$curdir/hlearn-allknn --varshift -r $INPUT -k 1 -n neighbors_hlearn.3.csv --distances-file=distances_hlearn.3.csv +RTS -K1000M -N2" \
+    "$curdir/hlearn-allknn --varshift -r $INPUT -k 1 -n neighbors_hlearn.4.csv --distances-file=distances_hlearn.4.csv +RTS -K1000M -N1" \
     "allknn -r $INPUT -k 1 -n neighbors_mlpack.1.csv -d distances_mlpack.1.csv -S " \
     "allknn -r $INPUT -k 1 -n neighbors_mlpack.2.csv -d distances_mlpack.2.csv " \
     "allknn -r $INPUT -k 1 -n neighbors_mlpack.3.csv -d distances_mlpack.3.csv -c S" \
